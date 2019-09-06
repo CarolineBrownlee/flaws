e27bdce6111d483addf058219ce2891395142ea7
Caroline Brownlee <brownleecaroline@gmail.com>
2019-09-06 12:29:11 -0500

    set up initial files for firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase/app";
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyApUmUjCUdJsqTialVEnbFAE5D_-NQNwoE",
+    authDomain: "flaws-a-dating-app.firebaseapp.com",
+    databaseURL: "https://flaws-a-dating-app.firebaseio.com",
+    projectId: "flaws-a-dating-app",
+    storageBucket: "flaws-a-dating-app.appspot.com",
+    messagingSenderId: "644539717172",
+    appId: "1:644539717172:web:54a6311598855824f41c61"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
